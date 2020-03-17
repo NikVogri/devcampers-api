8b31c24d5a31892e6d965c074af63275c7bdf661
Nik Vogrinec <vogrinec.nik@gmail.com>
2020-03-17 22:33:46 +0100

    added mongoose

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const dotenv = require("dotenv");
-
 const morgan = require("morgan");
+const colors = require("colors");
+const connectDatabase = require("./config/db");
 // Route files
 const bootcampsRoutes = require("./routes/bootcamps");
 
@@ -12,6 +13,9 @@ dotenv.config({
 
 const app = express();
 
+// Connect to database
+connectDatabase();
+
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
@@ -20,6 +24,17 @@ app.use("/api/v1", bootcampsRoutes);
 
 // server init
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () =>
-  console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`)
+const server = app.listen(PORT, () =>
+  console.log(
+    `Server running in ${process.env.NODE_ENV} on port ${PORT}`.yellow.bold
+  )
 );
+
+// Handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+  // Close server & exit process
+  server.close(() => {
+    process.exit(1);
+  });
+});
